2ce5b022648ff25d496e5fa34da30d8bb55db421
umadevisomal <umadevisomal@gmail.com>
2025-11-17 10:52:06 -0500

    pushing some mlflow config changes

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,11 +27,11 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+#dagshub.init(repo_owner='umadevisomal', repo_name='ETLpipeline-networksecurity-project', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/umadevisomal/ETLpipeline-networksecurity-project.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="umadevisomal"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="92ee269b83edabea71c868c2added4bfbd83c290"
 
 
 
@@ -46,7 +46,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        mlflow.set_registry_uri("https://dagshub.com/umadevisomal/ETLpipeline-networksecurity-project.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
